7da516c8e716b35073620b9ef5f1686c2f239f0e
Kundan baranwal <baranwalk624@gmail.com>
2026-01-01 21:57:28 +0530

    four

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import axios from "axios";
+
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
